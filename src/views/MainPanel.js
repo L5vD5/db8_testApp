9d8e6ab4e0c145184772128efa15595d61945163
L5vD5 <l5vd5@korea.ac.kr>
2018-12-08 01:38:08 +0900

    Update MainPanel.js

--- src/views/MainPanel.js
+++ src/views/MainPanel.js
@@ -9,16 +9,57 @@ import React from 'react';
 const handler = handle(
   // eslint-disable-next-line
   (ev, props) => {
-    console.log("Click");
     new LS2Request().send({
       service: 'luna://com.webos.service.db/',
       method: 'putKind',
       parameters: {
         id: 'com.domain.app:1',
-        owner: 'com.domain.app2',
-        indexes: [{name: 'sample', props: [{name: 'se'}]}]
+        owner: 'com.domain.app',
+        indexes: [{name: 'sample', props: [{name: 'sample'}]}]
       }
     });
+    let start = new Date().getTime();
+
+    for(let i=0; i<1000; i++) {
+      new LS2Request().send({
+        service: 'luna://com.webos.service.db/',
+        method: 'put',
+        parameters: {
+          objects: [
+            {
+              "_kind": 'com.domain.app:1',
+              sample: 'sample'+i,
+            }
+          ]
+        }
+      });
+    }
+
+    let elapsed = new Date().getTime() - start;
+    console.warn("l5vd5 put : " + elapsed);
+    let start2 = new Date().getTime();
+
+    for(let i=0; i<1000; i++) {
+      new LS2Request().send({
+        service: 'luna://com.webos.service.db/',
+        method: 'find',
+        parameters: {
+          query: {
+            from: 'com.domain.app:1',
+            where: [
+              {
+                prop: 'sample',
+                op: '=',
+                val: 'sample'+i,
+              }
+            ]
+          }
+        }
+      });
+    }
+
+    let elapsed2 = new Date().getTime() - start2;
+    console.warn("l5vd5 find  : " + elapsed2);
   }
 );
 
